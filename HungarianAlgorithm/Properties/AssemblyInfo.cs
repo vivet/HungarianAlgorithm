c78fa85700a3875657c21f9f002943e141473df8
Michael Vivet <vivet@vivetonline.com>
2016-07-29 18:29:03 +0200

    Changed .Net version to 4.5.1

--- HungarianAlgorithm/Properties/AssemblyInfo.cs
+++ HungarianAlgorithm/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HungarianAlgorithm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hungarian Algorithm")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
 [assembly: AssemblyProduct("HungarianAlgorithm")]
